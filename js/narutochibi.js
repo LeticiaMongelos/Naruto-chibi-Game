5570a9b80da22201643d35e804cc92255bbafa31
Leticia Mongelos <bethmongelos7@gmail.com>
2023-04-22 23:08:43 -0400

    Creación de clase para personajes

--- js/narutochibi.js
+++ js/narutochibi.js
@@ -1,34 +1,58 @@
+const characterPlayerButton = document.getElementById('button-character') 
+const fireButton = document.getElementById('fire-button')
+const waterButton = document.getElementById('water-button')
+const earthButton = document.getElementById('earth-button')
+const windButton = document.getElementById('wind-button')
+const lightningButton = document.getElementById('lightning-button')
+const sectionChoosingAtacck = document.getElementById('attack-choose')
+const sectionRestart = document.getElementById('restart')
+const restartButton = document.getElementById('restart-button')
+const characterRandomButton = document.getElementById('button-random')
+const sectionChoosingCharacter = document.getElementById('character-choose')
+const inputNaruto = document.getElementById('naruto')
+const inputMinato = document.getElementById('minato')
+const inputItachi = document.getElementById('itachi')
+const spanPlayerCharacter = document.getElementById('player-character')
+const spanRandomCharacter = document.getElementById('enemy-character')
+const spanPlayerLives = document.getElementById('player-lives')
+const spanEnemyLives = document.getElementById('enemy-lives')
+const result = document.getElementById('fight-result')
+const attackPlayer = document.getElementById('player-attack')
+const attackEnemy = document.getElementById('enemy-attack')
+
+let characters = []
 let playerAttack
 let enemyAttack
 let playerLives = 3;
 let enemyLives = 3;
 
+class Character {
+    constructor(name, pic, lives) {
+        this.name = name
+        this.pic = pic
+        this.lives = lives
+    }
+}
+
+let naruto = new Character('Naruto Uzumaki', './img/Naruto.png', 3)
+let minato = new Character('Minato Namikaze', './img/Minato.png', 3)
+let itachi = new Character('Itachi Uchiha', './img/Itachi.png', 3)
+
+characters.push(naruto, minato, itachi)
+
 function startGame() {
-    // Vinculación del elemento con su ID
-    let characterPlayerButton = document.getElementById('button-character') 
-    // Se agrega el "evento" esperado (el click)
     characterPlayerButton.addEventListener('click', choosingPlayerCharacter) 
-    let characterRandomButton = document.getElementById('button-random')
     characterRandomButton.addEventListener('click', randomPlayerCharacter)
 
-    let fireButton = document.getElementById('fire-button')
     fireButton.addEventListener('click', fireAttack)
-    let waterButton = document.getElementById('water-button')
     waterButton.addEventListener('click', waterAttack)
-    let earthButton = document.getElementById('earth-button')
     earthButton.addEventListener('click', earthAttack)
-    let windButton = document.getElementById('wind-button')
     windButton.addEventListener('click', windAttack)
-    let lightningButton = document.getElementById('lightning-button')
     lightningButton.addEventListener('click', lightningAttack)
 
-    let restartButton = document.getElementById('restart-button')
     restartButton.addEventListener('click', restartGame)
 
-    // Se ocultan las seciones hasta que corresponda utilizarlas
-    let sectionChoosingAtacck = document.getElementById('attack-choose')
     sectionChoosingAtacck.style.display = 'none'
-    let sectionRestart = document.getElementById('restart')
     sectionRestart.style.display = 'none'
 }
 
@@ -37,19 +61,10 @@ function random(min, max) {
 }
 
 function choosingPlayerCharacter() {
-    let sectionChoosingAtacck = document.getElementById('attack-choose')
     sectionChoosingAtacck.style.display = 'flex'
-    let sectionChoosingCharacter = document.getElementById('character-choose')
     sectionChoosingCharacter.style.display = 'none'
-    let sectionRestart = document.getElementById('restart')
     sectionRestart.style.display = 'none'
 
-    let inputNaruto = document.getElementById('naruto')
-    let inputMinato = document.getElementById('minato')
-    let inputItachi = document.getElementById('itachi')
-    let spanPlayerCharacter = document.getElementById('player-character')
-    
-    // innerHTML para modificar el DOM (para hacer dinámica partes de la estructura)
     if (inputNaruto.checked) {
         spanPlayerCharacter.innerHTML = 'Naruto Uzumaki'
         choosingEnemyCharacter()
@@ -63,16 +78,12 @@ function choosingPlayerCharacter() {
         alert('You must select a character to continue!')
     }
 
-    // Desactivación de los botones luego de usarlos
-    let characterPlayerButton = document.getElementById('button-character')
     characterPlayerButton.disabled = true
-    let characterRandomButton = document.getElementById('button-random')
     characterRandomButton.disabled = true
-}
+} 
 
 function randomPlayerCharacter() {
     let randomCharacter = random(1,3)
-    let spanPlayerCharacter = document.getElementById('player-character')
 
     if (randomCharacter == 1) {
         spanPlayerCharacter.innerHTML = 'Naruto Uzumaki'
@@ -82,15 +93,13 @@ function randomPlayerCharacter() {
         spanPlayerCharacter.innerHTML = 'Itachi Uchiha'
     }
     choosingEnemyCharacter()
-    let characterPlayerButton = document.getElementById('button-character')
+    
     characterPlayerButton.disabled = true
-    let characterRandomButton = document.getElementById('button-random')
     characterRandomButton.disabled = true
 }
 
 function choosingEnemyCharacter() {
     let randomCharacter = random(1,3)
-    let spanRandomCharacter = document.getElementById('enemy-character')
 
     if (randomCharacter == 1) {
         spanRandomCharacter.innerHTML = 'Naruto Uzumaki'
@@ -144,9 +153,6 @@ function lightningAttack() {
 }
 
 function fight() {
-    let spanPlayerLives = document.getElementById('player-lives')
-    let spanEnemyLives = document.getElementById('enemy-lives')
-
     if (playerAttack == enemyAttack) {
         createMessage("TIE")  
     } else if (playerAttack == 'KATON' && enemyAttack == 'FUUTON' ) {
@@ -186,11 +192,7 @@ function lifeCheck() {
 
 }
 
-// ANALIZA A FONDO COMO FUNCIONA ESTA PARTE
 function createMessage(fightResult) {
-    let result = document.getElementById('fight-result')
-    let attackPlayer = document.getElementById('player-attack')
-    let attackEnemy = document.getElementById('enemy-attack')
 
     let newPlayerAttack = document.createElement('p')
     let newEnemyAttack = document.createElement('p')
@@ -204,27 +206,21 @@ function createMessage(fightResult) {
 }
 
 function createFinalMessage(finalResult) {
-    let sectionMessages = document.getElementById('fight-result')
     
-    sectionMessages.innerHTML = finalResult 
+    result.innerHTML = finalResult 
     
-    let fireButton = document.getElementById('fire-button')
     fireButton.disabled = true
-    let waterButton = document.getElementById('water-button')
     waterButton.disabled = true
-    let earthButton = document.getElementById('earth-button')
     earthButton.disabled = true
-    let windButton = document.getElementById('wind-button')
     windButton.disabled = true
-    let lightningButton = document.getElementById('lightning-button')
     lightningButton.disabled = true
 
-    let sectionRestart = document.getElementById('restart')
     sectionRestart.style.display = 'flex'
 }
+
 function restartGame() {
     location.reload()
 }
-//window (navegador) llamar a cualquier evento que le pase al navegador, para no llevar el script hasta abajo
+
 window.addEventListener('load', startGame)
 
